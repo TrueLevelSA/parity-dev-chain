c2ae81bea339339001823170bdbc8e0b9b7c53c5
Nicolas Huguenin <nichuguen@gmail.com>
2018-11-27 15:08:17 +0100

    WIP account generation

---
+++ parity_docker/generate_validators_and_accounts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function generate_validators_and_accounts {
+    local -r NUMBER_TO_GENERATE=$1
+
+    ./start_dockers.sh 1
+    ./start_parity.sh
+    ./get_ips.sh
+
+    local -r IP=$(cat docker_ips.txt)
+
+    local -r USER_PATTERN=user
+    local -r USER_PWD_PATTERN=$USER_PATTERN
+
+    local -r NODE_PATTERN=node
+    local -r NODE_PWD_PATTERN=$NODE_PATTERN
+
+    python3 generate_validators_and_accounts.py $NUMBER_TO_GENERATE $IP $USER_PATTERN $USER_PWD_PATTERN $NODE_PATTERN $NODE_PWD_PATTERN
+
+    ./stop_dockers.sh
+}
+
+if [ $# != 1 ]
+then
+    echo "Usage: $0 number_of_containers
+    example: $0 5"
+    exit
+else
+    generate_validators_and_accounts $1
+    exit
+fi
+
+
---
+++ parity_docker/update_parity_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function update_parity_config {
+    local -r FILENAME=demo-spec.json
+    local -r CONTAINER_FILENAME=$FILENAME
+    for DOCKER_ID in $(docker-compose ps -q)
+    do
+        echo "Updating $FILENAME into $DOCKER_ID"
+
+        cat $FILENAME docker exec -i tee $CONTAINER_FILENAME > /dev/null
+    done
+}
+
+update_parity_config
